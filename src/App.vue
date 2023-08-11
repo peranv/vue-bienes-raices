435d828f70cb01e464b77f284b0f828bd184afc0
Esperanza Najera <esperanza.nave@gmail.com>
2023-08-11 10:46:34 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+   <v-card 
+    elevation="3"
+    max-width="1200"
+    class="mx-auto"
+   >
+
+   <v-layout>
+    <v-app-bar color="blue-darken-1">
+       <template v-slot:append>
+        
+       </template>
+       <template v-slot:prepend>
+        
+       </template>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <RouterView />
+      </v-container>
+    </v-main>
+
+    
+
+   </v-layout>
+        
+   </v-card>
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+   <h1 class="text-center text-h3 front-weight-bold my-5">Vue Bienes Raices</h1>
+  </main>
+</template>
