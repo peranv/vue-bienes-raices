9f6a1c49e2610ac70b75949ccdb69ec45f8841c4
Esperanza Najera <esperanza.nave@gmail.com>
2023-08-16 12:05:38 -0700

    Agregando formulario para agregar nueva propiedad

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -4,4 +4,11 @@
 
 <template>
   <h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
+  <v-btn
+     color="blue"
+     variant="flat"
+     :to="{name: 'nueva-propiedad'}"
+  >
+      Nueva Propiedad
+  </v-btn>
 </template>
--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,7 +1,77 @@
 <script setup>
-
+   const items = [1,2,3,4,5]
 </script>
 
 <template>
   <h2 class="text-center text-h3 my-5 font-weight-bold">Nueva propiedad</h2>
+
+  <v-car max-width="800" flat class="mx-auto my-10">
+    <v-card-title 
+       class="text-h4 font-weight-bold"
+       tag="h3"
+    >
+        Nueva Propiedad
+    </v-card-title>
+     <v-card-subtitle class="text-h5 py-5">
+        Crea una nueva propiedad llenando el siguiente formulario
+     </v-card-subtitle>
+
+     <v-form class="mt-10">
+        <v-text-field 
+           class="mb-5"
+           label="Titulo Propiedad"
+        />
+      <v-file-input
+         accept="image/jpeg"
+         label="Fotografía"
+         prepend-icon="mdi-camera"
+         class="mb-5"
+      />
+      <v-text-field 
+           class="mb-5"
+           label="Precio"
+        />
+      <v-row>
+       
+      <v-col
+         cols="12"
+         md="4"
+      >
+      <v-select 
+        label="Habitaciones"
+        class="mb-5"
+        :items="items"
+      />
+      </v-col>
+      <v-col
+         cols="12"
+         md="4"
+      >
+      <v-select 
+        label="WC"
+        class="mb-5"
+        :items="items"
+      />
+    </v-col>
+    <v-col
+         cols="12"
+         md="4"
+      >
+      <v-select 
+        label="Lugares Estacionamiento"
+        class="mb-5"
+        :items="items"
+      />
+    </v-col>
+  </v-row>
+    <v-textarea class="mb-5" label="Descripcion"></v-textarea>
+    <v-checkbox label="Alberca"  />
+    <v-btn
+       color="pink-accent-3"
+       block
+    >
+      Agregar Propiedad
+    </v-btn>
+     </v-form>
+  </v-car>
 </template>
